d2ae7f8fc7faba5b17c6de3bb6229ebe41dac2b6
Katya <13gordienkok@gmail.com>
2020-08-10 17:38:49 +0300

    Переводим строку в массив и выводим в консоль

--- script/script.js
+++ script/script.js
@@ -1,2 +1,26 @@
 let money = 10000;
 console.log(money);
+let income = {
+  websiteCreation: 2500,
+  lending: 1000
+};
+// let addExpenses = {
+//   internetTaxi: 600;
+//   utilities: 1200
+// };
+let addExpenses = 'Интернет такси 600 грн, Коммуналка 1200 грн';
+let denosit = true;
+let mission = 50000;
+let period = 4;
+let budgetDay = money / 30;
+
+console.log(budgetDay);
+console.log(money);
+console.log(income);
+console.log(denosit);
+
+addExpenses.length();
+console.log(period.setMonth());
+addExpenses.toLowerCase();
+addExpenses.split();
+console.log(addExpenses);
