483e1e802b48a876090adb2f841fca9132f9fa09
Katya <13gordienkok@gmail.com>
2020-08-11 22:09:11 +0300

    Исправила

--- script/script.js
+++ script/script.js
@@ -1,26 +1,22 @@
-let money = 10000;
+let money = 10500;
 console.log(money);
-let income = {
-  websiteCreation: 2500,
-  lending: 1000
-};
-// let addExpenses = {
-//   internetTaxi: 600;
-//   utilities: 1200
-// };
-let addExpenses = 'Интернет такси 600 грн, Коммуналка 1200 грн';
+let income = 'Фриланс 2500 грн';
+console.log(income);
+let addExpenses = `Интернет 230 грн, такси  600 грн,
+    Коммуналка  1200 грн`;
+addExpenses.length;
 let denosit = true;
 let mission = 50000;
 let period = 4;
 let budgetDay = money / 30;
-
 console.log(budgetDay);
-console.log(money);
+typeof (money);
+typeof (income);
+typeof (deposit);
 console.log(income);
 console.log(denosit);
-
-addExpenses.length();
-console.log(period.setMonth());
+console.log(mission + ' ' + 'гривен');
+console.log(addExpenses);
+console.log(period + ' ' + 'месяцев');
 addExpenses.toLowerCase();
 addExpenses.split();
-console.log(addExpenses);
